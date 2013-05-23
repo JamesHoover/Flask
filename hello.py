5d209fa29daf018dc923d3f52da1e43c9375f60c
James Hoover <jhoover@cequint.com>
2013-05-23 15:13:42 -0700

    Relocated files/cleaned up

---
+++ hello.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def helloName(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The projects page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

749e797175eac247605be5042bd9e3e3caa927db
James Hoover <jhoover@cequint.com>
2013-05-23 13:31:05 -0700

    First commit

---
+++ venv/hello.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def helloName(name):
+    return 'Hello %s' % name
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The projects page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
